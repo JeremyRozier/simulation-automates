713f0290d121fbaa72efda80667eb5d0bd63366e
BEAUMONT Léo <99603748+leobeaumont@users.noreply.github.com>
2023-02-17 17:38:56 +0100

    added features
    the automatons now works
    you can display an automaton using print()
    you can print the regular expression of an automaton with print(automaton.regex())

--- automaton_handler.py
+++ automaton_handler.py
@@ -1,13 +1,14 @@
 import random
+from automata.fa.nfa import NFA
+from automata.fa.gnfa import GNFA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
-class Automaton:
-    def __init__(self, alphabet, states, trans, init, final):
-        self.alphabet = alphabet
-        self.states = states
-        self.trans = trans
-        self.init = init
-        self.final = final
+class Automaton(NFA):
+    def __init__(self, states, input_symbols, transitions, initial_state, final_states):
+        super().__init__(states=states, input_symbols=input_symbols, transitions=transitions,
+                         initial_state=initial_state, final_states=final_states)
 
     @staticmethod
     def random_automaton(alphabet, min_states, max_states, trans_density, init_amount, final_amount, init_range=0,
@@ -28,12 +29,12 @@ class Automaton:
         """
         if seed is not None:
             random.seed(seed)
-        states = set()
+        states = {'init'}
         for i in range(random.randint(min_states, max_states)):
             states.add(i)
-        trans = dict()
-        for state_in in states:
-            for state_out in states:
+        trans = {'init': {'': set()}}
+        for state_in in states.difference({'init'}):
+            for state_out in states.difference({'init'}):
                 for char in alphabet:
                     if random.random() < trans_density:
                         try:
@@ -43,6 +44,26 @@ class Automaton:
                                 trans[state_in][char] = {state_out}
                             except KeyError:
                                 trans[state_in] = {char: {state_out}}
-        init = set(random.sample(states, init_amount + random.randint(-init_range, init_range)))
-        final = set(random.sample(states, final_amount + random.randint(-final_range, final_range)))
-        return states, trans, init, final
+        init = random.sample(states.difference({'init'}),
+                             max(init_amount + random.randint(-init_range, init_range), 1))
+        print(init)
+        for i in init:
+            trans['init'][''].add(i)
+        final = set(random.sample(states.difference({'init'}),
+                    final_amount + random.randint(-final_range, final_range)))
+        return Automaton(states, alphabet, trans, 'init', final)
+
+    def __str__(self):
+        try:
+            open('./display.png', "x")
+        except FileExistsError:
+            pass
+        self.show_diagram(path='./display.png')
+        plt.figure('Automate')
+        plt.axis('off')
+        plt.imshow(mpimg.imread('display.png'))
+        plt.show()
+        return super.__str__(self)
+
+    def regex(self):
+        return GNFA.from_nfa(self).to_regex()
