a6476627a30446ab507e22f2793b35835cb566dc
JeremyRozier <jeremy.rozier70@gmail.com>
2023-02-20 00:46:50 +0100

    Added pyproject.toml file for black configs with line length set to 79 chars | automaton_handler.py : Removed two methods make_complete and get_random_accepted_words. Reformat docstring for fixing linting errors.

--- automaton_handler.py
+++ automaton_handler.py
@@ -8,7 +8,14 @@ import numpy as np
 
 
 class Automaton(NFA):
-    def __init__(self, states, input_symbols, transitions, initial_state, final_states):
+    def __init__(
+        self,
+        states,
+        input_symbols,
+        transitions,
+        initial_state,
+        final_states,
+    ):
         super().__init__(
             states=states,
             input_symbols=input_symbols,
@@ -23,28 +30,33 @@ class Automaton(NFA):
 
     @staticmethod
     def random_automaton(
-            alphabet,
-            min_states,
-            max_states,
-            trans_density,
-            init_amount,
-            final_amount,
-            init_range=0,
-            final_range=0,
-            seed=None,
+        alphabet,
+        min_states,
+        max_states,
+        trans_density,
+        init_amount,
+        final_amount,
+        init_range=0,
+        final_range=0,
+        seed=None,
     ):
         """
-        This function creates a random automaton with desired probabilities
+        This function creates a random
+        automaton with desired probabilities
         :param alphabet: set of the alphabet used by the automaton (set of str)
         :param min_states: minimum amount of states (int)
         :param max_states: maximum amount of states (int)
-        :param trans_density: chance for a transition to be created (percentage)
+        :param trans_density: chance for a transition
+        to be created (percentage)
         :param init_amount: amount of initial states (int)
         :param final_amount: amount of final states (int)
-        :param init_range: maximal variation in the amount of initial states (int)
-        :param final_range: maximal variation in the amount of final states (int)
+        :param init_range: maximal variation
+        in the amount of initial states (int)
+        :param final_range: maximal variation in
+        the amount of final states (int)
         :param seed: random seed, None by default
-        :return: a set of states, a dictionary of transition, set of initial states, set of final states
+        :return: a set of states, a dictionary of transition,
+        set of initial states, set of final states
         """
         if seed is not None:
             random.seed(seed)
@@ -64,17 +76,35 @@ class Automaton(NFA):
                             except KeyError:
                                 trans[state_in] = {char: {state_out}}
         init = set()
-        for i in range(max(init_amount + random.randint(-init_range, init_range), 1)):
+        for i in range(
+            max(
+                init_amount + random.randint(-init_range, init_range),
+                1,
+            )
+        ):
             if len(states.difference(init.union({"init"}))) > 0:
-                init.add(random.choice(list(states.difference(init.union({"init"})))))
+                init.add(
+                    random.choice(
+                        list(states.difference(init.union({"init"})))
+                    )
+                )
             else:
                 break
         for i in init:
             trans["init"][""].add(i)
         final = set()
-        for i in range(max(final_amount + random.randint(-final_range, final_range), 0)):
+        for i in range(
+            max(
+                final_amount + random.randint(-final_range, final_range),
+                0,
+            )
+        ):
             if len(states.difference(final.union({"init"}))) > 0:
-                final.add(random.choice(list(states.difference(final.union({"init"})))))
+                final.add(
+                    random.choice(
+                        list(states.difference(final.union({"init"})))
+                    )
+                )
             else:
                 break
         return Automaton(states, alphabet, trans, "init", final)
@@ -91,78 +121,29 @@ class Automaton(NFA):
         plt.show()
         return super.__str__(self)
 
-    def get_random_accepted_words(self, nb):
-        """
-        Generate nb different accepted words
-        :param nb: number of words to generate (int)
-        :return: list of generated words (list)
-        """
-        reachable_states = self._compute_reachable_states(
-            self.initial_state, self.input_symbols, self.transitions
-        )
-        if not any([state in self.final_states for state in reachable_states]):
-            print("The language of this automaton is the empty gather.")
-            return None
-        regex = self.get_regex()
-        if regex == "":
-            return [""]
-        if "*" not in regex and "+" not in regex:
-            max_words = exrex.count(regex)
-            if nb > max_words:
-                nb = max_words
-        list_words = list()
-        same = 0
-        while len(list_words) < nb:
-            buffer = list_words.copy()
-            generated_word = exrex.getone(regex, limit=30)
-            if generated_word not in list_words:
-                list_words.append(generated_word)
-            if buffer == list_words:
-                same += 1
-            else:
-                same = 0
-            if same >= 100:
-                break
-        return list_words
-
     def classify_words(self, nb):
         """
-        This function creates a 2 dimensional array with shape (np, 2) where the first column is a word in the alphabet
-        and the second column is "1" if the word on the same row is accepted by the automaton and "0" otherwise
+        This function creates a 2 dimensional array with shape (np, 2) where
+        the first column is a word in the alphabet
+        and the second column is "1" if the word on the same row is accepted
+        by the automaton and "0" otherwise
         :param nb: number of word in the array
         :return: 2 dimensional array with shape (np, 2) and datatype=str
         """
         classified = 0
-        sigma_star = Automaton({0},
-                               self.input_symbols,
-                               {0: {char: {0} for char in self.input_symbols}},
-                               0,
-                               {0}).get_regex()
+        sigma_star = Automaton(
+            {0},
+            self.input_symbols,
+            {0: {char: {0} for char in self.input_symbols}},
+            0,
+            {0},
+        ).get_regex()
         classification = []
         for word in exrex.generate(sigma_star, limit=100):
             classification.append([word, int(self.accepts_input(word))])
             classified += 1
             if classified >= nb:
                 return np.array(classification)
-
-    def make_complete(self):
-        """
-        this function makes an equivalent complete NFA
-        :return: None
-        """
-        states = set(self.states)
-        states.add("well")
-        transitions = dict()
-        for key in self.transitions.keys():
-            transitions[key] = {char: set(self.transitions[key][char]) for char in self.transitions[key].keys()}
-        for state in states.difference({"init"}):
-            if state not in transitions.keys():
-                transitions[state] = {char: {"well"} for char in self.input_symbols}
-            else:
-                for char in self.input_symbols:
-                    if char not in transitions[state].keys():
-                        transitions[state][char] = {"well"}
-        return Automaton(states, self.input_symbols, transitions, self.initial_state, self.final_states)
 
     def get_regex(self):
         return GNFA.from_nfa(self).to_regex()
@@ -178,7 +159,19 @@ if __name__ == "__main__":
     init_range = 1
     final_range = 1
     automaton = Automaton.random_automaton(
-        alphabet, min_states, max_states, trans_density, init_amount, final_amount, init_range, final_range
+        alphabet,
+        min_states,
+        max_states,
+        trans_density,
+        init_amount,
+        final_amount,
+        init_range,
+        final_range,
+    )
+    print(
+        [
+            automaton.accepts_input(word)
+            for word in automaton.get_random_declined_words(10)
+        ]
     )
     print(automaton)
-    
