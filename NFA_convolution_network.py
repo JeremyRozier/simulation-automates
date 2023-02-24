9b88d7a0b11e87135adb4e7502a8a2bda15435db
BEAUMONT Léo <99603748+leobeaumont@users.noreply.github.com>
2023-02-24 18:41:04 +0100

    first version of the model is now usable
    working with pre-filled in the function values, need to be tested with other values.

--- NFA_convolution_network.py
+++ NFA_convolution_network.py
@@ -1,6 +1,8 @@
 import numpy as np
 from automaton_handler import Automaton
-import tensorflow as ts
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 
 
 class Automaton2Network:
@@ -12,22 +14,39 @@ class Automaton2Network:
         """
         self.automaton = automaton
         self.x_train, self.y_train = self.automaton.classify_words(train_set_size)
-        self.network = None
 
-    def create_network(self, nb_layers=32, nb_kernels=4, pooling_kernel=4):
-        model = ts.keras.Sequential()
-        model.add(ts.keras.layers.Conv1D(nb_layers,
-                                         nb_kernels,
-                                         input_shape=(np.shape(self.x_train[0]))
-                                         )
-                  )
-        model.add(ts.keras.layers.Activation(ts.keras.activations.relu))
-        model.add(ts.keras.layers.MaxPooling1D(pool_size=pooling_kernel))
-        model.add(ts.keras.layers.Flatten())
-        model.add(ts.keras.layers.Dense(units=2))
-        model.add(ts.keras.layers.Activation(ts.keras.activations.softmax))
-        model.compile(loss="categorical_crossentropy",
-                      optimizer="adam",
-                      metrics=["accuracy"])
-        model.fit(self.x_train, self.y_train, epochs=50, batch_size=32, validation_split=0.2)
-        self.network = model
+    def create_model(self, nb_layers=32, nb_kernels=4, pooling_kernel=4, epochs_amount=15, save=True):
+        """
+        This function creates a sequential model reproducing the behavior of the automaton
+        :param nb_layers: amount of different nodes used for convolution (int)
+        :param nb_kernels: number of entry accessible by each node in the convolution layer (int)
+        :param pooling_kernel: number of node affected to one pooling node (int)
+        :param epochs_amount: number of iteration of the training sequence (int)
+        :param save: choose if the model is saved or not after generation (bool)
+        :return: None
+        """
+        print("Creating model for the regular expression: {}".format(self.automaton.get_regex()))
+        inp = np.shape(self.x_train[0])
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Conv1D(nb_layers, nb_kernels, input_shape=inp, padding='same'))
+        model.add(tf.keras.layers.Activation(tf.keras.activations.relu))
+        model.add(tf.keras.layers.MaxPooling1D(pool_size=pooling_kernel, padding='same'))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(units=1))
+        model.add(tf.keras.layers.Activation(tf.keras.activations.sigmoid))
+        print(model.summary())
+        model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+        model.fit(self.x_train, self.y_train, epochs=epochs_amount, batch_size=32, validation_split=0.2)
+        if save:
+            num = len([name for name in os.listdir('./stored_models')]) + 1
+            model.save("./stored_models/saved_model_{}".format(num))
+            print("Model saved at 'stored_models/saved_model_{}'".format(num))
+
+    @staticmethod
+    def load_model(number):
+        """
+        This function loads a saved model
+        :param number: number of the saved model
+        :return: usable model
+        """
+        return tf.keras.models.load_model("./stored_models/saved_model_{}".format(number))
