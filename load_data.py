fd24cd8e424067012babcc9cc0f887ca4c0621cf
JeremyRozier <jeremy.rozier70@gmail.com>
2023-04-19 23:28:27 +0200

    Fixed bugs in load_data.py

--- load_data.py
+++ load_data.py
@@ -7,42 +7,42 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 print("loading statrting")
 
-# Set up the paths to the stored automata and the CSV file
-csv_file_path = "simulation-automates/automaton_accuracies.csv"
+is_automaton_found = False
 
+# Set up the paths to the stored automata and the CSV file
+csv_file_path = "automaton_accuracies.csv"
 if os.path.exists(csv_file_path):
     with open(csv_file_path, "r") as f:
         # Créer un objet csv à partir du fichier
         obj = csv.reader(f)
         last_line = [line for line in obj][-1]
         name_last_automaton = last_line[1]
-        if name_last_automaton == "Automata Name":
-            name_last_automaton = ""
+        size_last_automaton = int(last_line[0])
 else:
     with open(csv_file_path, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["Size", "Automata Name", "Accuracy"])
-    name_last_automaton = ""
-print(name_last_automaton)
+    is_automaton_found = True
 # Create a list of all the automata file names and their sizes
 automata_files = []
-size_range = range(2, 49)
-is_automaton_found = False
+size_range = range(1, 49)
 for size in size_range:
     name_indexes = (0, 101)
-    folder_path = f"simulation-automates/stored_automatons/size_{size}"
+    folder_path = f"stored_automatons/size_{size}"
     automata_names = os.listdir(folder_path)
     if (
-        len(name_last_automaton) != 0
-        and name_last_automaton not in automata_names
-        and not is_automaton_found
+        not is_automaton_found
+        and size == size_last_automaton
+        and name_last_automaton in automata_names
     ):
-        continue
-    elif name_last_automaton in automata_names:
         is_automaton_found = True
         index_last_automaton = automata_names.index(name_last_automaton)
         name_indexes = (index_last_automaton + 1, 101)
-    for name in automata_names[name_indexes[0]:name_indexes[1]]:
+
+    elif not is_automaton_found:
+        continue
+
+    for name in automata_names[name_indexes[0] : name_indexes[1]]:
         automata_files.append((size, os.path.join(folder_path, name)))
 # Create a list to hold the accuracy and file name for each automaton
 accuracy_list = []
@@ -55,9 +55,8 @@ for size, file in automata_files:
     file_name = os.path.basename(file)
     accuracy_list.append((size, os.path.basename(file_name), acc))
     compteur += 1
-    print((size, os.path.basename(file_name)))
     if compteur % 10 == 0:
-        print('writing')
+        print("writing")
         with open(csv_file_path, mode="a", newline="") as file:
             writer = csv.writer(file)
             for accuracy in accuracy_list:
